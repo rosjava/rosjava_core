0b4677316be7fbf7f2f8289175bffdc153556e1c
Ethan Rublee <ethan.rublee@gmail.com>
2011-03-02 15:29:18 -0800

    Adding copyright.

--- apps/RosCamera/src/org/ros/android/camera/RosCamera.java
+++ apps/RosCamera/src/org/ros/android/camera/RosCamera.java
@@ -1,9 +1,23 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.ros.android.camera;
 
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
-import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,21 +30,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import org.ros.Node;
-import org.ros.Publisher;
-import org.ros.exceptions.RosInitException;
-import org.ros.exceptions.RosNameException;
-import org.ros.message.sensor.Image;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
 import java.util.List;
 
-public class RosCamera extends Activity{
-  private static final String ROS_CAMERA_TAG = "RosCamera";
+/**
+ * @author ethan.rublee@gmail.com (Ethan Rublee)
+ * 
+ */
+public class RosCamera extends Activity {
   private Preview mPreview;
 
   Camera mCamera;
@@ -38,7 +46,7 @@ public class RosCamera extends Activity{
 
   // The first rear facing camera
   int defaultCameraId;
-  
+
   RosCameraNode cameraNode;
 
   @Override
@@ -53,10 +61,6 @@ public class RosCamera extends Activity{
     // and set it as the content of our activity.
     mPreview = new Preview(this);
     setContentView(mPreview);
-
-    if (cameraNode == null)
-      cameraNode = new RosCameraNode();
-
   }
 
   @Override
@@ -67,6 +71,7 @@ public class RosCamera extends Activity{
     mCamera = Camera.open();
     cameraCurrentlyLocked = defaultCameraId;
     mPreview.setCamera(mCamera);
+    cameraNode = new RosCameraNode("android_camera");
     mCamera.setPreviewCallback(cameraNode);
   }
 
@@ -102,7 +107,6 @@ public class RosCamera extends Activity{
       return super.onOptionsItemSelected(item);
     }
   }
-
 
 }
 
--- apps/RosCamera/src/org/ros/android/camera/RosCameraNode.java
+++ apps/RosCamera/src/org/ros/android/camera/RosCameraNode.java
@@ -1,4 +1,25 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.ros.android.camera;
+
+import org.ros.RosContext;
+
+import org.ros.Ros;
+
+import org.ros.message.sensor.CameraInfo;
 
 import org.ros.message.Time;
 
@@ -23,41 +44,32 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
+/** A camera node that publishes images and camera_info
+ * @author ethan.rublee@gmail.com (Ethan Rublee)
+ *
+ */
 public class RosCameraNode implements PreviewCallback {
   private static final String ROS_CAMERA_TAG = "RosCamera";
   Node node;
   private Publisher<Image> imagePublisher;
   private Publisher<org.ros.message.std.String> talker;
   private int seq;
+  private Publisher<CameraInfo> cameraInfoPublisher;
 
-  public String getLocalIpAddress() {
-    try {
-      for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en
-          .hasMoreElements();) {
-        NetworkInterface intf = en.nextElement();
-        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr
-            .hasMoreElements();) {
-          InetAddress inetAddress = enumIpAddr.nextElement();
-          // IPv4 only for now
-          if (!inetAddress.isLoopbackAddress() && inetAddress.getAddress().length == 4) {
-            return inetAddress.getHostAddress().toString();
-          }
-        }
-      }
-    } catch (SocketException ex) {
-      Log.e(ROS_CAMERA_TAG, ex.toString());
-    }
-    return null;
-  }
 
-  public RosCameraNode() {
+  /**
+   * @param node_name the camera node name
+   */
+  public RosCameraNode(String node_name) {
     // Find the total number of cameras available
     try {
-      node = new Node("camera");
-      Log.i(ROS_CAMERA_TAG, "My name is what? " + getLocalIpAddress());
-      node.init("http://10.0.129.167:11311", getLocalIpAddress());
-      imagePublisher = node.createPublisher("image_color", Image.class);
-      talker = node.createPublisher("image_talker", org.ros.message.std.String.class);
+      node = new Node(node_name, Ros.getDefaultContext());
+      Log.i(ROS_CAMERA_TAG, "My name is what? " + Ros.getLocalIpAddress());
+      //FIXME resolve rosmaster from some global properties.
+      node.init("http://10.0.129.167:11311", Ros.getLocalIpAddress());
+      imagePublisher = node.createPublisher("~image_raw", Image.class);
+      cameraInfoPublisher = node.createPublisher("~camera_info", CameraInfo.class);
+      talker = node.createPublisher("~talker", org.ros.message.std.String.class);
     } catch (RosNameException e) {
       // TODO Auto-generated catch block
       Log.e(ROS_CAMERA_TAG, e.getMessage());
