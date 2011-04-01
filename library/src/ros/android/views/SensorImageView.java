a31798a979d466c442722e284e8f871924df3e8a
Ethan Rublee <ethan.rublee@gmail.com>
2011-03-31 20:30:12 -0700

    ImageSub -> SensorImageView

--- library/src/ros/android/views/SensorImageView.java
+++ library/src/ros/android/views/SensorImageView.java
@@ -15,15 +15,21 @@
  */
 package ros.android.views;
 
+import android.graphics.Color;
+
+import android.util.Log;
+
+import org.ros.message.sensor_msgs.Image;
+
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.util.Log;
+import android.graphics.BitmapFactory;
+import android.util.AttributeSet;
 import android.widget.ImageView;
 import org.ros.MessageListener;
 import org.ros.Node;
 import org.ros.exceptions.RosInitException;
-import org.ros.message.sensor_msgs.Image;
+import org.ros.message.sensor_msgs.CompressedImage;
 
 /**
  * A camera node that publishes images and camera_info
@@ -31,16 +37,27 @@ import org.ros.message.sensor_msgs.Image;
  * @author ethan.rublee@gmail.com (Ethan Rublee)
  * 
  */
-public class ImageSub extends ImageView implements MessageListener<Image>, Runnable {
-  private org.ros.Subscriber<Image> imageSub;
+public class SensorImageView extends ImageView implements MessageListener<CompressedImage>,
+    Runnable {
+  private org.ros.Subscriber<CompressedImage> imageSub;
 
-  public ImageSub(Context ctx) {
+  public SensorImageView(Context ctx) {
     super(ctx);
     // bitmap = Bitmap.createBitmap(width, height, config)
   }
 
-  public void init(Node node,String topic) throws RosInitException {
-    imageSub = node.createSubscriber(topic, this, Image.class);
+  public SensorImageView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+    // TODO Auto-generated constructor stub
+  }
+
+  public SensorImageView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    // TODO Auto-generated constructor stub
+  }
+
+  public void init(Node node, String topic) throws RosInitException {
+    imageSub = node.createSubscriber(topic, this, CompressedImage.class);
   }
 
   public void stop() {
@@ -50,30 +67,37 @@ public class ImageSub extends ImageView implements MessageListener<Image>, Runna
   Bitmap bitmap;
 
   @Override
-  public void onNewMessage(Image message) {
+  public void onNewMessage(CompressedImage message) {
+    bitmap = BitmapFactory.decodeByteArray(message.data, 0, message.data.length);
+    post(this);
+  }
+
+  /**
+   * FIXME make this switchable.
+   * 
+   * @param message
+   */
+  public void decodeNonCompressed(Image message) {
     // FIXME support more encodings.
-    if (message.encoding != "rgb8") {
+    if (!message.encoding.equals("rgb8")) {
       Log.e("RosAndroid", "Unsopported encoding : " + message.encoding);
       return;
     }
-    //if the bitmap is a different size or null allocate it.
+    // if the bitmap is a different size or null allocate it.
     if (bitmap == null || bitmap.getWidth() != message.width
         || bitmap.getHeight() != message.height) {
       bitmap = Bitmap.createBitmap((int) message.width, (int) message.height,
           Bitmap.Config.ARGB_8888);
     }
-    
-    //copy the message data into the bitmap.
+    // copy the message data into the bitmap.
     for (int x = 0; x < message.width; x++) {
       for (int y = 0; y < message.height; y++) {
         byte red = message.data[(int) (y * message.step + x)];
         byte green = message.data[(int) (y * message.step + x + 1)];
         byte blue = message.data[(int) (y * message.step + x + 2)];
-        bitmap.setPixel(x, y, Color.argb(1, red, green, blue));
+        bitmap.setPixel(x, y, Color.argb(1, blue, green, red));
       }
     }
-    post(this);
-
   }
 
   @Override
@@ -81,7 +105,5 @@ public class ImageSub extends ImageView implements MessageListener<Image>, Runna
     setImageBitmap(bitmap);
 
   }
-
- 
 
 }
