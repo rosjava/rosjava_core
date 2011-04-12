847c819449fa0dff90c7ec67e53e12d773a9d8f7
Ken Conley <kwc@kwc.org>
2011-04-12 15:14:14 -0700

    Switched to callback style and adjusted API for new app_manager srv structures.

--- java/src/org/ros/app_manager/AppManager.java
+++ java/src/org/ros/app_manager/AppManager.java
@@ -32,19 +32,14 @@
  */
 package org.ros.app_manager;
 
-import org.ros.namespace.NameResolver;
-
 import org.ros.MessageListener;
 import org.ros.Node;
 import org.ros.internal.node.service.ServiceClient;
 import org.ros.internal.node.service.ServiceIdentifier;
-import org.ros.message.Message;
-import org.ros.message.app_manager.App;
+import org.ros.namespace.NameResolver;
 import org.ros.service.app_manager.ListApps;
 import org.ros.service.app_manager.StartApp;
 import org.ros.service.app_manager.StopApp;
-
-import java.util.ArrayList;
 
 //TODO(kwc) this class is not meant to be part of rosjava and is only being developed here until rosjava matures
 /**
@@ -65,13 +60,14 @@ public class AppManager {
     listAppsClient = null;
     stopAppClient = null;
     startAppClient = null;
-    
+
     resolver = node.getResolver().createResolver(robotName);
   }
 
   private void initListApps() throws AppManagerNotAvailableException {
     if (listAppsClient == null) {
-      ServiceIdentifier serviceIdentifier = node.lookupService(resolver.resolveName("list_apps"), new ListApps());
+      ServiceIdentifier serviceIdentifier = node.lookupService(resolver.resolveName("list_apps"),
+          new ListApps());
       if (serviceIdentifier == null) {
         throw new AppManagerNotAvailableException();
       }
@@ -81,7 +77,8 @@ public class AppManager {
 
   private void initStartApp() throws AppManagerNotAvailableException {
     if (startAppClient == null) {
-      ServiceIdentifier serviceIdentifier = node.lookupService(resolver.resolveName("start_app"), new StartApp());
+      ServiceIdentifier serviceIdentifier = node.lookupService(resolver.resolveName("start_app"),
+          new StartApp());
       if (serviceIdentifier == null) {
         throw new AppManagerNotAvailableException();
       }
@@ -91,7 +88,8 @@ public class AppManager {
 
   private void initStopApp() throws AppManagerNotAvailableException {
     if (stopAppClient == null) {
-      ServiceIdentifier serviceIdentifier = node.lookupService(resolver.resolveName("stop_app"), new StopApp());
+      ServiceIdentifier serviceIdentifier = node.lookupService(resolver.resolveName("stop_app"),
+          new StopApp());
       if (serviceIdentifier == null) {
         throw new AppManagerNotAvailableException();
       }
@@ -99,81 +97,25 @@ public class AppManager {
     }
   }
 
-  static class ResponseListener<T extends Message> implements MessageListener<T> {
-    private T response;
-
-    ResponseListener() {
-      response = null;
-    }
-
-    @Override
-    public void onNewMessage(T message) {
-      System.out.println("got response");
-      response = message;
-    }
-
-    public T getResponse() {
-      return response;
-    }
-
-    public void waitForResponse() {
-      // TODO: need equivalent of node.ok()
-      while (getResponse() == null) {
-        try {
-          Thread.sleep(10);
-        } catch (InterruptedException e) {
-        }
-      }
-    }
-  }
-
-  private ListApps.Response callListApps() throws AppManagerNotAvailableException {
+  public void listApps(MessageListener<ListApps.Response> callback)
+      throws AppManagerNotAvailableException {
     initListApps();
-    ListApps.Request request = new ListApps.Request();
-
-    ResponseListener<ListApps.Response> listener = new ResponseListener<ListApps.Response>();
-    listAppsClient.call(request, listener);
-    listener.waitForResponse();
-    return listener.getResponse();
+    listAppsClient.call(new ListApps.Request(), callback);
   }
 
-  private StartApp.Response callStartApp(String appName) throws AppManagerNotAvailableException {
+  public void startApp(String appName, MessageListener<StartApp.Response> callback)
+      throws AppManagerNotAvailableException {
     initStartApp();
     StartApp.Request request = new StartApp.Request();
     request.name = appName;
-    
-    ResponseListener<StartApp.Response> listener = new ResponseListener<StartApp.Response>();
-    startAppClient.call(request, listener);
-    listener.waitForResponse();
-    return listener.getResponse();
+    startAppClient.call(request, callback);
   }
 
-  private StopApp.Response callStopApp() throws AppManagerNotAvailableException {
+  public void stopApp(String appName, MessageListener<StopApp.Response> callback)
+      throws AppManagerNotAvailableException {
     initStopApp();
     StopApp.Request request = new StopApp.Request();
-
-    ResponseListener<StopApp.Response> listener = new ResponseListener<StopApp.Response>();
-    stopAppClient.call(request, listener);
-    listener.waitForResponse();
-    return listener.getResponse();
-  }
-
-  public ArrayList<App> getRunningApps() throws AppManagerNotAvailableException {
-    ListApps.Response response = callListApps();
-    return response.running_apps;
-  }
-
-  public ArrayList<App> getAvailableApps() throws AppManagerNotAvailableException {
-    ListApps.Response response = callListApps();
-    return response.available_apps;
-  }
-
-  public StartApp.Response startApp(String appName) throws AppManagerNotAvailableException {
-    return callStartApp(appName);
-  }
-
-  public void stopApp(String appName) throws AppManagerNotAvailableException {
-    // TODO: pass in app name
-    callStopApp();
+    request.name = appName;
+    stopAppClient.call(request, callback);
   }
 }
--- java/src/org/ros/app_manager/TestAppManager.java
+++ java/src/org/ros/app_manager/TestAppManager.java
@@ -1,13 +1,12 @@
 package org.ros.app_manager;
 
-import org.ros.service.app_manager.StartApp;
-
-import org.ros.message.app_manager.App;
-
 import org.apache.commons.logging.Log;
 import org.ros.Node;
 import org.ros.NodeContext;
 import org.ros.NodeMain;
+import org.ros.message.app_manager.App;
+import org.ros.service.app_manager.ListApps;
+import org.ros.service.app_manager.StartApp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,26 +22,33 @@ public class TestAppManager implements NodeMain {
       final Log log = node.getLog();
       log.info("Creating app manager");
       appManager = new AppManager(node, "robot1");
-      
-      ArrayList<App> availableApps = appManager.getAvailableApps();
-      for (App app: availableApps) { 
-        log.info("Available app: "+app.display_name);
+
+      log.info("Listing apps");
+      BasicMessageListener<ListApps.Response> callback = new BasicMessageListener<ListApps.Response>();
+      appManager.listApps(callback);
+      ArrayList<App> availableApps = callback.waitForResponse(10 * 1000).available_apps;
+      for (App app : availableApps) {
+        log.info("Available app: " + app.display_name);
       }
-      
-      ArrayList<App> runningApps = appManager.getRunningApps();
-      for (App app: runningApps) { 
-        log.info("Running app: "+app.display_name);
+      log.info("Getting running apps");
+      appManager.listApps(callback);
+      ArrayList<App> runningApps = callback.waitForResponse(10 * 1000).running_apps;
+      for (App app : runningApps) {
+        log.info("Running app: " + app.display_name);
       }
-      
+
       log.info("calling start app");
-      StartApp.Response startResponse = appManager.startApp("foo/fakeApp");
+      BasicMessageListener<StartApp.Response> startCallback = new BasicMessageListener<StartApp.Response>();
+      appManager.startApp("foo/fakeApp", startCallback);
+      StartApp.Response startResponse = startCallback.waitForResponse(10 * 1000);
       log.info("start app called");
-      if (!startResponse.started) { 
-        log.info("fake app failed to start (this is good).  response message is: "+startResponse.message);
-      } else { 
+      if (!startResponse.started) {
+        log.info("fake app failed to start (this is good).  response message is: "
+            + startResponse.message);
+      } else {
         log.error("fake app started, this is weird");
       }
-      
+
     } catch (Exception e) {
       if (node != null) {
         node.getLog().fatal(e);
