3db46f4fafbf546343e5a8838f16cd45b9f59a7f
Damon Kohler <damonkohler@google.com>
2011-04-19 13:22:28 +0200

    Add skeleton for Android pubsub tutorial.

---
+++ android/tutorials/pubsub/src/org/ros/tutorials/pubsub/MainActivity.java
@@ -0,0 +1,13 @@
+package org.ros.tutorials.pubsub;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
