a3146cd9ff667b156ab208e064d9946c2ade8321
Damon Kohler <damonkohler@google.com>
2011-05-26 14:27:00 +0200

    Remove generated local.properties files. Update hgignore. Change create_project.sh to run across all android tutorials.

--- android/tutorials/create_project.sh
+++ android/tutorials/create_project.sh
@@ -1,3 +1,12 @@
 # There's an NPE bug that makes it necessary to split this up in the case of an empty project.
-android update project --target 11 --path .
-android update project --target 11 --path . --library ../../library
+function update {
+  android update project --target 11 --path $1
+  android update project --target 11 --path $1 --library ../../library
+}
+
+update "RosCamera"
+update "Teleop"
+update "image_transport"
+update "orientation_publisher"
+update "pan_tilt_camera"
+update "pubsub"
