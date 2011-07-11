740b3779f80301e7d432a18903115476b824b75f
Damon Kohler <damonkohler@google.com>
2011-07-11 18:01:21 +0200

    Pull out NodeFactory.

--- rosjava/src/org/ros/Ros.java
+++ rosjava/src/org/ros/Ros.java
@@ -22,6 +22,7 @@ import org.ros.namespace.GraphName;
 import org.ros.namespace.NameResolver;
 import org.ros.node.Node;
 import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,16 +32,37 @@ import java.util.Map;
  */
 public class Ros {
 
+  private static NodeFactory nodeFactory;
+
+  private static final class DefaultNodeFactory implements NodeFactory {
+    @Override
+    public Node newNode(GraphName name, NodeConfiguration configuration) {
+      return new DefaultNode(name, configuration);
+    }
+  }
+
+  static {
+    setNodeFactory(new DefaultNodeFactory());
+  }
+
   private Ros() {
     // Utility class
   }
 
+  public static NodeFactory getNodeFactory() {
+    return nodeFactory;
+  }
+
+  public static void setNodeFactory(NodeFactory nodeFactory) {
+    Ros.nodeFactory = nodeFactory;
+  }
+
   public static Node newNode(GraphName name, NodeConfiguration configuration) {
-    return new DefaultNode(name, configuration);
+    return nodeFactory.newNode(name, configuration);
   }
 
   public static Node newNode(String name, NodeConfiguration configuration) {
-    return newNode(new GraphName(name), configuration);
+    return nodeFactory.newNode(new GraphName(name), configuration);
   }
 
   public static NameResolver newNameResolver(GraphName namespace,
