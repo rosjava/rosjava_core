0dbd524d30b92e134a65d62582c4d300fc40c363
Damon Kohler <damonkohler@google.com>
2011-11-10 10:23:05 +0100

    Fixes issue 70. We now depend on the newest Android SDK/ADT.

--- rosjava_bootstrap/src/main/python/rosjava_bootstrap/make.py
+++ rosjava_bootstrap/src/main/python/rosjava_bootstrap/make.py
@@ -36,48 +36,48 @@ def _usage():
 def _write_sorted_properties(properties, stream=sys.stdout):
     for key in sorted(properties):
         print >>stream, '%s=%s' % (key, properties[key])
-        
-           
+
+
 def _remove(path):
     try:
         os.remove(path)
     except:
         print 'Failed to remove %r' % path
-            
-            
+
+
 def _run(command, checked=True):
     print 'Executing command: %r' % command
-    retcode = subprocess.call(command)            
+    retcode = subprocess.call(command)
     if retcode and checked:
         sys.exit(retcode)
-            
-            
+
+
 def build(rospack, package):
     with open('dependencies.xml', 'w') as stream:
         maven.write_ant_maven_dependencies(rospack, package, stream)
-        
+
     maven_depmap = maven.get_maven_dependencies(package, 'dependencies.xml')
-    
+
     properties = ros_properties.generate(rospack, package, maven_depmap)
     with open('ros.properties', 'w') as stream:
         _write_sorted_properties(properties, stream)
-            
+
     properties = android.generate_properties(rospack, package)
     if properties is not None:
-        with open('default.properties', 'w') as stream:
+        with open('project.properties', 'w') as stream:
             _write_sorted_properties(properties, stream)
-                
+
     generate_msg_depends.generate_msg_depends(package)
-    
+
     with open('.classpath', 'w') as stream:
         eclipse.write_classpath(rospack, package, maven_depmap, stream)
 
     with open('.project', 'w') as stream:
         eclipse.write_project(package, stream)
-            
+
     _run(['ant'])
-            
-            
+
+
 def clean():
     _run(['ant', 'clean'], checked=False)
     _remove('dependencies.xml')
@@ -85,12 +85,12 @@ def clean():
     _remove('default.properties')
     _remove('.classpath')
     _remove('.project')
-    
+
 
 def test():
     _run(['ant', 'test'])
 
-        
+
 def main(argv):
     if len(argv) < 2:
         _usage()
@@ -102,7 +102,7 @@ def main(argv):
         clean()
     elif argv[2] == 'test':
         test()
-    
+
 
 if __name__ == '__main__':
     main(sys.argv)
