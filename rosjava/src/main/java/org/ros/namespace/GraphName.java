4c0e734d94e54314f5e33b8c183dfcecf2cfd630
Damon Kohler <damonkohler@google.com>
2012-01-11 15:19:19 +0100

    Change hashCode and equals of GraphName to the typical Eclipse autogenerated version.

--- rosjava/src/main/java/org/ros/namespace/GraphName.java
+++ rosjava/src/main/java/org/ros/namespace/GraphName.java
@@ -277,17 +277,32 @@ public class GraphName {
   }
 
   @Override
-  public boolean equals(Object obj) {
-    return name.equals(obj.toString());
+  public String toString() {
+    return name;
   }
 
   @Override
   public int hashCode() {
-    return name.hashCode();
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
   }
 
   @Override
-  public String toString() {
-    return name;
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    GraphName other = (GraphName) obj;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    return true;
   }
 }
