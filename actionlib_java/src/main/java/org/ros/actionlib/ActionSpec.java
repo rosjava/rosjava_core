1f2041ad98a1028bc42ad33730ce11042c332a75
Keith Hughes <khughes@google.com>
2012-01-19 12:19:51 -0500

    Licensing put on actionlib.

--- actionlib_java/src/main/java/org/ros/actionlib/ActionSpec.java
+++ actionlib_java/src/main/java/org/ros/actionlib/ActionSpec.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2011 Alexander Perzylo, Technische Universität München
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.ros.actionlib;
 
 import org.ros.actionlib.client.ActionClient;
@@ -13,7 +29,6 @@ import org.ros.message.Time;
 import org.ros.message.actionlib_msgs.GoalID;
 import org.ros.message.actionlib_msgs.GoalStatus;
 import org.ros.message.std_msgs.Header;
-import org.ros.node.Node;
 
 import java.lang.reflect.Field;
 
