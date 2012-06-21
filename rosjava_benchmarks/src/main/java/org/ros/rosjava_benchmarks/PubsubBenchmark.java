c7737dbfa00de39a5bba0fe487816ba4ef2d7de0
Damon Kohler <damonkohler@google.com>
2012-06-21 15:13:34 +0200

    Added rosjava_benchmarks package. Currently only contains a single benchmark that reports the maximum rate that tfMessages that can be pushed from a publisher to a subscriber.

---
+++ rosjava_benchmarks/src/main/java/org/ros/rosjava_benchmarks/PubsubBenchmark.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ros.rosjava_benchmarks;
+
+import org.ros.concurrent.CancellableLoop;
+import org.ros.message.Duration;
+import org.ros.message.MessageListener;
+import org.ros.message.Time;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Publisher;
+import org.ros.node.topic.Subscriber;
+import tf.tfMessage;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author damonkohler@google.com (Damon Kohler)
+ */
+public class PubsubBenchmark extends AbstractNodeMain {
+
+  private final AtomicInteger counter;
+
+  private Publisher<std_msgs.String> statusPublisher;
+  private Publisher<tf.tfMessage> tfPublisher;
+  private Subscriber<tf.tfMessage> tfSubscriber;
+  private Time time;
+
+  public PubsubBenchmark() {
+    counter = new AtomicInteger();
+  }
+
+  @Override
+  public GraphName getDefaultNodeName() {
+    return new GraphName("pubsub_benchmark");
+  }
+
+  @Override
+  public void onStart(final ConnectedNode connectedNode) {
+    time = connectedNode.getCurrentTime();
+    statusPublisher = connectedNode.newPublisher("status", std_msgs.String._TYPE);
+    tfPublisher = connectedNode.newPublisher("tf", tf.tfMessage._TYPE);
+    tfSubscriber = connectedNode.newSubscriber("tf", tf.tfMessage._TYPE);
+    final std_msgs.String status = statusPublisher.newMessage();
+    tfSubscriber.addMessageListener(new MessageListener<tf.tfMessage>() {
+      @Override
+      public void onNewMessage(tfMessage message) {
+        counter.incrementAndGet();
+        Time now = connectedNode.getCurrentTime();
+        Duration delta = now.subtract(time);
+        if (delta.totalNsecs() > TimeUnit.NANOSECONDS.convert(5, TimeUnit.SECONDS)) {
+          double hz = counter.getAndSet(0) * 1e9 / delta.totalNsecs();
+          status.setData(String.format("%.2f Hz", hz));
+          statusPublisher.publish(status);
+          time = now;
+        }
+      }
+    });
+    connectedNode.executeCancellableLoop(new CancellableLoop() {
+      @Override
+      protected void loop() throws InterruptedException {
+        final tf.tfMessage tfMessage = tfPublisher.newMessage();
+        geometry_msgs.TransformStamped transformStamped =
+            connectedNode.getTopicMessageFactory()
+                .newFromType(geometry_msgs.TransformStamped._TYPE);
+        tfMessage.getTransforms().add(transformStamped);
+        tfPublisher.publish(tfMessage);
+      }
+    });
+  }
+}
