da4c92b3f165ce1f041c7d981cf705876cd52b7a
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-10 00:37:39 +0300

    Added final modifiers

--- rosjava/src/main/java/org/ros/internal/node/response/ProtocolDescriptionResultFactory.java
+++ rosjava/src/main/java/org/ros/internal/node/response/ProtocolDescriptionResultFactory.java
@@ -29,14 +29,14 @@ import com.google.common.base.Preconditions;
 /**
  * @author damonkohler@google.com (Damon Kohler)
  */
-public class ProtocolDescriptionResultFactory implements ResultFactory<ProtocolDescription> {
+public final class ProtocolDescriptionResultFactory implements ResultFactory<ProtocolDescription> {
 
   @Override
-  public ProtocolDescription newFromValue(Object value) {
-    List<Object> protocolParameters = Arrays.asList((Object[]) value);
+  public final ProtocolDescription newFromValue(Object value) {
+    final List<Object> protocolParameters = Arrays.asList((Object[]) value);
     Preconditions.checkState(protocolParameters.size() == 3);
     Preconditions.checkState(protocolParameters.get(0).equals(ProtocolNames.TCPROS));
-    AdvertiseAddress address = new AdvertiseAddress((String) protocolParameters.get(1));
+    final AdvertiseAddress address = new AdvertiseAddress((String) protocolParameters.get(1));
     address.setStaticPort((Integer) protocolParameters.get(2));
     return new TcpRosProtocolDescription(address);
   }
