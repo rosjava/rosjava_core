2f530583fc188ae4d34780522a055cb0bb7f0452
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-17 00:16:15 +0300

    Replace apache logging with SL4j

--- apache_xmlrpc_server/src/main/java/org/apache/xmlrpc/server/XmlRpcErrorLogger.java
+++ apache_xmlrpc_server/src/main/java/org/apache/xmlrpc/server/XmlRpcErrorLogger.java
@@ -18,8 +18,8 @@
  */
 package org.apache.xmlrpc.server;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -27,7 +27,7 @@ import org.apache.commons.logging.LogFactory;
  * error logging.
  */
 public class XmlRpcErrorLogger {
-    private static final Log log = LogFactory.getLog(XmlRpcErrorLogger.class);
+    private static final Logger log = LoggerFactory.getLogger(XmlRpcErrorLogger.class);
 
     /**
      * Called to log the given error.
