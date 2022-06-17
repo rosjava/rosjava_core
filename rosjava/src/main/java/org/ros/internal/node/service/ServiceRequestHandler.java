53fc76a75a578c85318c365c6e5c8ef3390727da
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-17 20:25:34 +0300

    Use StandardCharsets.US_ASCII

--- rosjava/src/main/java/org/ros/internal/node/service/ServiceRequestHandler.java
+++ rosjava/src/main/java/org/ros/internal/node/service/ServiceRequestHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -31,6 +31,7 @@ import org.ros.node.service.ServiceResponseBuilder;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -38,77 +39,71 @@ import java.util.concurrent.ExecutorService;
  */
 class ServiceRequestHandler<T extends Message, S extends Message> extends SimpleChannelHandler {
 
-  private final ServiceDeclaration serviceDeclaration;
-  private final ServiceResponseBuilder<T, S> responseBuilder;
-  private final MessageDeserializer<T> deserializer;
-  private final MessageSerializer<S> serializer;
-  private final MessageFactory messageFactory;
-  private final ExecutorService executorService;
-  private final MessageBufferPool messageBufferPool;
+    private final ServiceDeclaration serviceDeclaration;
+    private final ServiceResponseBuilder<T, S> responseBuilder;
+    private final MessageDeserializer<T> deserializer;
+    private final MessageSerializer<S> serializer;
+    private final MessageFactory messageFactory;
+    private final ExecutorService executorService;
+    private final MessageBufferPool messageBufferPool = new MessageBufferPool();
+    ;
 
-  public ServiceRequestHandler(ServiceDeclaration serviceDeclaration,
-      ServiceResponseBuilder<T, S> responseBuilder, MessageDeserializer<T> deserializer,
-      MessageSerializer<S> serializer, MessageFactory messageFactory,
-      ExecutorService executorService) {
-    this.serviceDeclaration = serviceDeclaration;
-    this.deserializer = deserializer;
-    this.serializer = serializer;
-    this.responseBuilder = responseBuilder;
-    this.messageFactory = messageFactory;
-    this.executorService = executorService;
-    messageBufferPool = new MessageBufferPool();
-  }
+    public ServiceRequestHandler(ServiceDeclaration serviceDeclaration,
+                                 ServiceResponseBuilder<T, S> responseBuilder, MessageDeserializer<T> deserializer,
+                                 MessageSerializer<S> serializer, MessageFactory messageFactory,
+                                 ExecutorService executorService) {
+        this.serviceDeclaration = serviceDeclaration;
+        this.deserializer = deserializer;
+        this.serializer = serializer;
+        this.responseBuilder = responseBuilder;
+        this.messageFactory = messageFactory;
+        this.executorService = executorService;
+    }
 
-  private void handleRequest(ChannelBuffer requestBuffer, ChannelBuffer responseBuffer)
-      throws ServiceException {
-    T request = deserializer.deserialize(requestBuffer);
-    S response = messageFactory.newFromType(serviceDeclaration.getType());
-    responseBuilder.build(request, response);
-    serializer.serialize(response, responseBuffer);
-  }
+    private void handleRequest(ChannelBuffer requestBuffer, ChannelBuffer responseBuffer)
+            throws ServiceException {
+      final T request = deserializer.deserialize(requestBuffer);
+      final S response = messageFactory.newFromType(serviceDeclaration.getType());
+      this.responseBuilder.build(request, response);
+      this.serializer.serialize(response, responseBuffer);
+    }
 
-  private void handleSuccess(final ChannelHandlerContext ctx, ServiceServerResponse response,
-      ChannelBuffer responseBuffer) {
-    response.setErrorCode(1);
-    response.setMessageLength(responseBuffer.readableBytes());
-    response.setMessage(responseBuffer);
-    ctx.getChannel().write(response);
-  }
+    private void handleSuccess(final ChannelHandlerContext ctx, ServiceServerResponse response,
+                               ChannelBuffer responseBuffer) {
+        response.setErrorCode(1);
+        response.setMessageLength(responseBuffer.readableBytes());
+        response.setMessage(responseBuffer);
+        ctx.getChannel().write(response);
+    }
 
-  private void handleError(final ChannelHandlerContext ctx, ServiceServerResponse response,
-      String message) {
-    response.setErrorCode(0);
-    ByteBuffer encodedMessage = Charset.forName("US-ASCII").encode(message);
-    response.setMessageLength(encodedMessage.limit());
-    response.setMessage(ChannelBuffers.wrappedBuffer(encodedMessage));
-    ctx.getChannel().write(response);
-  }
+    private void handleError(final ChannelHandlerContext ctx, ServiceServerResponse response,
+                             String message) {
+        response.setErrorCode(0);
+        final ByteBuffer encodedMessage = StandardCharsets.US_ASCII.encode(message);
+        response.setMessageLength(encodedMessage.limit());
+        response.setMessage(ChannelBuffers.wrappedBuffer(encodedMessage));
+        ctx.getChannel().write(response);
+    }
 
-  @Override
-  public void messageReceived(final ChannelHandlerContext ctx, MessageEvent e) throws Exception {
-    // Although the ChannelHandlerContext is explicitly documented as being safe
-    // to keep for later use, the MessageEvent is not. So, we make a defensive
-    // copy of the ChannelBuffer.
-    final ChannelBuffer requestBuffer = ((ChannelBuffer) e.getMessage()).copy();
-    executorService.execute(new Runnable() {
-      @Override
-      public void run() {
-        ServiceServerResponse response = new ServiceServerResponse();
-        ChannelBuffer responseBuffer = messageBufferPool.acquire();
-        boolean success;
-        try {
-          handleRequest(requestBuffer, responseBuffer);
-          success = true;
-        } catch (ServiceException ex) {
-          handleError(ctx, response, ex.getMessage());
-          success = false;
-        }
-        if (success) {
-          handleSuccess(ctx, response, responseBuffer);
-        }
-        messageBufferPool.release(responseBuffer);
-      }
-    });
-    super.messageReceived(ctx, e);
-  }
+    @Override
+    public void messageReceived(final ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+        // Although the ChannelHandlerContext is explicitly documented as being safe
+        // to keep for later use, the MessageEvent is not. So, we make a defensive
+        // copy of the ChannelBuffer.
+        final ChannelBuffer requestBuffer = ((ChannelBuffer) e.getMessage()).copy();
+        this.executorService.execute(() -> {
+            final ServiceServerResponse response = new ServiceServerResponse();
+            final ChannelBuffer responseBuffer = messageBufferPool.acquire();
+
+            try {
+                handleRequest(requestBuffer, responseBuffer);
+                handleSuccess(ctx, response, responseBuffer);
+            } catch (final ServiceException ex) {
+                handleError(ctx, response, ex.getMessage());
+            }
+
+            this.messageBufferPool.release(responseBuffer);
+        });
+        super.messageReceived(ctx, e);
+    }
 }
