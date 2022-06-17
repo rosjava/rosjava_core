7a2adf2d1827eb8725b15bb8ffe5620d37bef91b
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-17 21:35:24 +0300

    Added final modifier

--- rosjava/src/main/java/org/ros/internal/node/client/Client.java
+++ rosjava/src/main/java/org/ros/internal/node/client/Client.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,6 +16,8 @@
 
 package org.ros.internal.node.client;
 
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang.StringUtils;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
@@ -29,55 +31,54 @@ import java.net.URI;
 
 /**
  * Base class for XML-RPC clients (e.g. MasterClient and SlaveClient).
- * 
+ *
+ * @param <T> the XML-RPC interface this {@link Client} connects to
+ *
  * @author damonkohler@google.com (Damon Kohler)
- * 
- * @param <T>
- *          the XML-RPC interface this {@link Client} connects to
  */
 abstract class Client<T extends XmlRpcEndpoint> {
 
-  // TODO(damonkohler): This should be pulled out into a user configurable
-  // strategy.
-  private static final int CONNECTION_TIMEOUT = 60 * 1000; // 60 seconds
-  private static final int REPLY_TIMEOUT = 60 * 1000; // 60 seconds
-  private static final int XMLRPC_TIMEOUT = 10 * 1000; // 10 seconds
+    // TODO(damonkohler): This should be pulled out into a user configurable
+    // strategy.
+    private static final int CONNECTION_TIMEOUT = 60 * 1000; // 60 seconds
+    private static final int REPLY_TIMEOUT = 60 * 1000; // 60 seconds
+    private static final int XMLRPC_TIMEOUT = 10 * 1000; // 10 seconds
 
-  private final URI uri;
+    private final URI uri;
 
-  protected final T xmlRpcEndpoint;
+    protected final T xmlRpcEndpoint;
 
-  /**
-   * @param uri
-   *          the {@link URI} to connect to
-   * @param interfaceClass
-   *          the class literal for the XML-RPC interface
-   */
-  public Client(URI uri, Class<T> interfaceClass) {
-    this.uri = uri;
-    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-    try {
-      config.setServerURL(uri.toURL());
-    } catch (MalformedURLException e) {
-      throw new RosRuntimeException(e);
+    /**
+     * @param uri            the {@link URI} to connect to
+     * @param interfaceClass the class literal for the XML-RPC interface
+     */
+    public Client(URI uri, Class<T> interfaceClass) {
+        Preconditions.checkNotNull(uri);
+        Preconditions.checkNotNull(interfaceClass);
+        this.uri = uri;
+        final XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        try {
+            config.setServerURL(uri.toURL());
+        } catch (MalformedURLException e) {
+            throw new RosRuntimeException(e);
+        }
+        config.setConnectionTimeout(CONNECTION_TIMEOUT);
+        config.setReplyTimeout(REPLY_TIMEOUT);
+
+        final XmlRpcClient client = new XmlRpcClient();
+        client.setTransportFactory(new XmlRpcCommonsTransportFactory(client));
+        client.setConfig(config);
+
+        final XmlRpcClientFactory<T> factory = new XmlRpcClientFactory<T>(client);
+        this.xmlRpcEndpoint =
+                interfaceClass.cast(factory.newInstance(getClass().getClassLoader(), interfaceClass, StringUtils.EMPTY,
+                        XMLRPC_TIMEOUT));
     }
-    config.setConnectionTimeout(CONNECTION_TIMEOUT);
-    config.setReplyTimeout(REPLY_TIMEOUT);
 
-    XmlRpcClient client = new XmlRpcClient();
-    client.setTransportFactory(new XmlRpcCommonsTransportFactory(client));
-    client.setConfig(config);
-
-    XmlRpcClientFactory<T> factory = new XmlRpcClientFactory<T>(client);
-    xmlRpcEndpoint =
-        interfaceClass.cast(factory.newInstance(getClass().getClassLoader(), interfaceClass, "",
-            XMLRPC_TIMEOUT));
-  }
-
-  /**
-   * @return the {@link URI} of the remote {@link XmlRpcServer}
-   */
-  public URI getRemoteUri() {
-    return uri;
-  }
+    /**
+     * @return the {@link URI} of the remote {@link XmlRpcServer}
+     */
+    public final URI getRemoteUri() {
+        return uri;
+    }
 }
--- rosjava/src/test/java/org/ros/node/service/ServiceIntegrationTest.java
+++ rosjava/src/test/java/org/ros/node/service/ServiceIntegrationTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,17 +16,9 @@
 
 package org.ros.node.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 import org.ros.RosTest;
-import org.ros.exception.DuplicateServiceException;
-import org.ros.exception.RemoteException;
-import org.ros.exception.RosRuntimeException;
-import org.ros.exception.ServiceException;
-import org.ros.exception.ServiceNotFoundException;
+import org.ros.exception.*;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
@@ -34,158 +26,165 @@ import org.ros.node.ConnectedNode;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.*;
+
 /**
  * @author damonkohler@google.com (Damon Kohler)
  */
 public class ServiceIntegrationTest extends RosTest {
 
-  private static final String SERVICE_NAME = "/add_two_ints";
+    private static final String SERVICE_NAME = "/add_two_ints";
 
-  @Test
-  public void testPesistentServiceConnection() throws Exception {
-    final CountDownServiceServerListener<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> countDownServiceServerListener =
-        CountDownServiceServerListener.newDefault();
-    nodeMainExecutor.execute(new AbstractNodeMain() {
-      @Override
-      public GraphName getDefaultNodeName() {
-        return GraphName.of("server");
-      }
+    @Test
+    public void testPesistentServiceConnection() throws Exception {
+        final CountDownServiceServerListener<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> countDownServiceServerListener =
+                CountDownServiceServerListener.newDefault();
+        nodeMainExecutor.execute(new AbstractNodeMain() {
+            @Override
+            public GraphName getDefaultNodeName() {
+                return GraphName.of("server");
+            }
 
-      @Override
-      public void onStart(final ConnectedNode connectedNode) {
-        ServiceServer<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceServer =
-            connectedNode
-                .newServiceServer(
-                    SERVICE_NAME,
-                    rosjava_test_msgs.AddTwoInts._TYPE,
-                        (request, response) -> response.setSum(request.getA() + request.getB()));
-        try {
-          connectedNode.newServiceServer(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE, null);
-          fail();
-        } catch (DuplicateServiceException e) {
-          // Only one ServiceServer with a given name can be created.
-        }
-        serviceServer.addListener(countDownServiceServerListener);
-      }
-    }, nodeConfiguration);
+            @Override
+            public void onStart(final ConnectedNode connectedNode) {
+                ServiceServer<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceServer =
+                        connectedNode
+                                .newServiceServer(
+                                        SERVICE_NAME,
+                                        rosjava_test_msgs.AddTwoInts._TYPE,
+                                        (request, response) -> response.setSum(request.getA() + request.getB()));
+                try {
+                    connectedNode.newServiceServer(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE, null);
+                    fail();
+                } catch (DuplicateServiceException e) {
+                    // Only one ServiceServer with a given name can be created.
+                }
+                serviceServer.addListener(countDownServiceServerListener);
+            }
+        }, nodeConfiguration);
 
-    assertTrue(countDownServiceServerListener.awaitMasterRegistrationSuccess(1, TimeUnit.SECONDS));
+        assertTrue(countDownServiceServerListener.awaitMasterRegistrationSuccess(1, TimeUnit.SECONDS));
 
-    final CountDownLatch latch = new CountDownLatch(2);
-    nodeMainExecutor.execute(new AbstractNodeMain() {
-      @Override
-      public GraphName getDefaultNodeName() {
-        return GraphName.of("client");
-      }
+        final CountDownLatch latch = new CountDownLatch(2);
+        nodeMainExecutor.execute(new AbstractNodeMain() {
+            @Override
+            public GraphName getDefaultNodeName() {
+                return GraphName.of("client");
+            }
 
-      @Override
-      public void onStart(ConnectedNode connectedNode) {
-        ServiceClient<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceClient;
-        try {
-          serviceClient = connectedNode.newServiceClient(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE);
-          // Test that requesting another client for the same service returns
-          // the same instance.
-          ServiceClient<?, ?> duplicate =
-              connectedNode.newServiceClient(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE);
-          assertEquals(serviceClient, duplicate);
-        } catch (ServiceNotFoundException e) {
-          throw new RosRuntimeException(e);
-        }
-        rosjava_test_msgs.AddTwoIntsRequest request = serviceClient.newMessage();
-        request.setA(2);
-        request.setB(2);
-        serviceClient.call(request, new ServiceResponseListener<rosjava_test_msgs.AddTwoIntsResponse>() {
-          @Override
-          public void onSuccess(rosjava_test_msgs.AddTwoIntsResponse response) {
-            assertEquals(response.getSum(), 4);
-            latch.countDown();
-          }
+            @Override
+            public void onStart(ConnectedNode connectedNode) {
+                ServiceClient<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceClient;
+                try {
+                    serviceClient = connectedNode.newServiceClient(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE);
+                    // Test that requesting another client for the same service returns
+                    // the same instance.
+                    ServiceClient<?, ?> duplicate =
+                            connectedNode.newServiceClient(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE);
+                    assertEquals(serviceClient, duplicate);
+                } catch (ServiceNotFoundException e) {
+                    throw new RosRuntimeException(e);
+                }
+             final rosjava_test_msgs.AddTwoIntsRequest request = serviceClient.newMessage();
+                {
+//                   final rosjava_test_msgs.AddTwoIntsRequest request = serviceClient.newMessage();
+                    request.setA(2);
+                    request.setB(2);
+                    serviceClient.call(request, new ServiceResponseListener<rosjava_test_msgs.AddTwoIntsResponse>() {
+                        @Override
+                        public void onSuccess(rosjava_test_msgs.AddTwoIntsResponse response) {
+                            assertEquals(response.getSum(), 4);
+                            latch.countDown();
+                        }
 
-          @Override
-          public void onFailure(RemoteException e) {
-            throw new RuntimeException(e);
-          }
-        });
+                        @Override
+                        public void onFailure(RemoteException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+                {
+                    // Regression test for issue 122.
+//                    final rosjava_test_msgs.AddTwoIntsRequest request = serviceClient.newMessage();
+                    request.setA(3);
+                    request.setB(3);
+                    serviceClient.call(request, new ServiceResponseListener<rosjava_test_msgs.AddTwoIntsResponse>() {
+                        @Override
+                        public void onSuccess(rosjava_test_msgs.AddTwoIntsResponse response) {
+                            assertEquals(response.getSum(), 6);
+                            latch.countDown();
+                        }
 
-        // Regression test for issue 122.
-        request.setA(3);
-        request.setB(3);
-        serviceClient.call(request, new ServiceResponseListener<rosjava_test_msgs.AddTwoIntsResponse>() {
-          @Override
-          public void onSuccess(rosjava_test_msgs.AddTwoIntsResponse response) {
-            assertEquals(response.getSum(), 6);
-            latch.countDown();
-          }
+                        @Override
+                        public void onFailure(RemoteException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+            }
+        }, nodeConfiguration);
 
-          @Override
-          public void onFailure(RemoteException e) {
-            throw new RuntimeException(e);
-          }
-        });
-      }
-    }, nodeConfiguration);
+        assertTrue(latch.await(1, TimeUnit.SECONDS));
+    }
 
-    assertTrue(latch.await(1, TimeUnit.SECONDS));
-  }
+    @Test
+    public void testRequestFailure() throws Exception {
+        final String errorMessage = "Error!";
+        final CountDownServiceServerListener<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> countDownServiceServerListener =
+                CountDownServiceServerListener.newDefault();
+        nodeMainExecutor.execute(new AbstractNodeMain() {
+            @Override
+            public GraphName getDefaultNodeName() {
+                return GraphName.of("server");
+            }
 
-  @Test
-  public void testRequestFailure() throws Exception {
-    final String errorMessage = "Error!";
-    final CountDownServiceServerListener<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> countDownServiceServerListener =
-        CountDownServiceServerListener.newDefault();
-    nodeMainExecutor.execute(new AbstractNodeMain() {
-      @Override
-      public GraphName getDefaultNodeName() {
-        return GraphName.of("server");
-      }
+            @Override
+            public void onStart(ConnectedNode connectedNode) {
+                ServiceServer<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceServer =
+                        connectedNode
+                                .newServiceServer(
+                                        SERVICE_NAME,
+                                        rosjava_test_msgs.AddTwoInts._TYPE,
+                                        (request, response) -> {
+                                            throw new ServiceException(errorMessage);
+                                        });
+                serviceServer.addListener(countDownServiceServerListener);
+            }
+        }, nodeConfiguration);
 
-      @Override
-      public void onStart(ConnectedNode connectedNode) {
-        ServiceServer<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceServer =
-            connectedNode
-                .newServiceServer(
-                    SERVICE_NAME,
-                    rosjava_test_msgs.AddTwoInts._TYPE,
-                        (request, response) -> {
-                          throw new ServiceException(errorMessage);
-                        });
-        serviceServer.addListener(countDownServiceServerListener);
-      }
-    }, nodeConfiguration);
+        assertTrue(countDownServiceServerListener.awaitMasterRegistrationSuccess(1, TimeUnit.SECONDS));
 
-    assertTrue(countDownServiceServerListener.awaitMasterRegistrationSuccess(1, TimeUnit.SECONDS));
+        final CountDownLatch latch = new CountDownLatch(1);
+        nodeMainExecutor.execute(new AbstractNodeMain() {
+            @Override
+            public GraphName getDefaultNodeName() {
+                return GraphName.of("client");
+            }
 
-    final CountDownLatch latch = new CountDownLatch(1);
-    nodeMainExecutor.execute(new AbstractNodeMain() {
-      @Override
-      public GraphName getDefaultNodeName() {
-        return GraphName.of("client");
-      }
+            @Override
+            public void onStart(ConnectedNode connectedNode) {
+                ServiceClient<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceClient;
+                try {
+                    serviceClient = connectedNode.newServiceClient(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE);
+                } catch (ServiceNotFoundException e) {
+                    throw new RosRuntimeException(e);
+                }
+                rosjava_test_msgs.AddTwoIntsRequest request = serviceClient.newMessage();
+                serviceClient.call(request, new ServiceResponseListener<rosjava_test_msgs.AddTwoIntsResponse>() {
+                    @Override
+                    public void onSuccess(rosjava_test_msgs.AddTwoIntsResponse message) {
+                        fail();
+                    }
 
-      @Override
-      public void onStart(ConnectedNode connectedNode) {
-        ServiceClient<rosjava_test_msgs.AddTwoIntsRequest, rosjava_test_msgs.AddTwoIntsResponse> serviceClient;
-        try {
-          serviceClient = connectedNode.newServiceClient(SERVICE_NAME, rosjava_test_msgs.AddTwoInts._TYPE);
-        } catch (ServiceNotFoundException e) {
-          throw new RosRuntimeException(e);
-        }
-        rosjava_test_msgs.AddTwoIntsRequest request = serviceClient.newMessage();
-        serviceClient.call(request, new ServiceResponseListener<rosjava_test_msgs.AddTwoIntsResponse>() {
-          @Override
-          public void onSuccess(rosjava_test_msgs.AddTwoIntsResponse message) {
-            fail();
-          }
+                    @Override
+                    public void onFailure(RemoteException e) {
+                        assertEquals(e.getMessage(), errorMessage);
+                        latch.countDown();
+                    }
+                });
+            }
+        }, nodeConfiguration);
 
-          @Override
-          public void onFailure(RemoteException e) {
-            assertEquals(e.getMessage(), errorMessage);
-            latch.countDown();
-          }
-        });
-      }
-    }, nodeConfiguration);
-
-    assertTrue(latch.await(1, TimeUnit.SECONDS));
-  }
+        assertTrue(latch.await(1, TimeUnit.SECONDS));
+    }
 }
