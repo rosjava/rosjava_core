84ab9a040dc98b52e94f5b4ab92176b7d17ff27f
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-17 19:52:17 +0300

    Additional, event methods, Types extends Message

--- rosjava/src/main/java/org/ros/node/service/ServiceServer.java
+++ rosjava/src/main/java/org/ros/node/service/ServiceServer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,44 +16,50 @@
 
 package org.ros.node.service;
 
+import org.ros.internal.message.Message;
 import org.ros.namespace.GraphName;
 
 import java.net.URI;
 
 /**
  * Provides a ROS service.
- * 
- * @see <a href="http://www.ros.org/wiki/Services">Services documentation</a>
- * 
+ *
+ * @param <T> this {@link ServiceServer} responds to requests of this type
+ * @param <S> this {@link ServiceServer} returns responses of this type
+ *
  * @author damonkohler@google.com (Damon Kohler)
- * 
- * @param <T>
- *          this {@link ServiceServer} responds to requests of this type
- * @param <S>
- *          this {@link ServiceServer} returns responses of this type
+ * @see <a href="http://www.ros.org/wiki/Services">Services documentation</a>
  */
-public interface ServiceServer<T, S> {
+public interface ServiceServer<T extends Message, S extends Message > {
 
-  /**
-   * @return the name of the {@link ServiceServer}
-   */
-  GraphName getName();
+    /**
+     * @return the name of the {@link ServiceServer}
+     */
+    GraphName getName();
 
-  /**
-   * @return the {@link URI} for this {@link ServiceServer}
-   */
-  URI getUri();
+    /**
+     * @return the {@link URI} for this {@link ServiceServer}
+     */
+    URI getUri();
 
-  /**
-   * Stops the service and unregisters it.
-   */
-  void shutdown();
+    /**
+     * Stops the service and unregisters it.
+     */
+    void shutdown();
 
-  /**
-   * Add a {@link ServiceServerListener}.
-   * 
-   * @param listener
-   *          the {@link ServiceServerListener} to add
-   */
-  void addListener(ServiceServerListener<T, S> listener);
+    /**
+     * Add a {@link ServiceServerListener}.
+     *
+     * @param listener the {@link ServiceServerListener} to add
+     */
+    void addListener(ServiceServerListener<T, S> listener);
+
+    void onMasterRegistrationSuccess();
+
+    void onMasterRegistrationFailure();
+
+    void onMasterUnregistrationSuccess();
+
+    void onMasterUnregistrationFailure();
+
 }
