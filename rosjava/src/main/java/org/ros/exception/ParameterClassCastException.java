74d3e60c0d5001590e68cbb4122324390ba1846a
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2022-06-17 21:34:20 +0300

    Remove unused methods

--- rosjava/src/main/java/org/ros/exception/ParameterClassCastException.java
+++ rosjava/src/main/java/org/ros/exception/ParameterClassCastException.java
@@ -22,17 +22,13 @@ package org.ros.exception;
  * 
  * @author damonkohler@google.com (Damon Kohler)
  */
-public class ParameterClassCastException extends RosRuntimeException {
+public final class ParameterClassCastException extends RosRuntimeException {
 
-  public ParameterClassCastException(String message) {
-    super(message);
-  }
+
 
   public ParameterClassCastException(String message, Throwable throwable) {
     super(message, throwable);
   }
 
-  public ParameterClassCastException(Throwable throwable) {
-    super(throwable);
-  }
+
 }
